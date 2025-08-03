a44712245f84bf6046a322a23ed7db1c58dce92d
Kamogelo <ekmogasoa@gmail.com>
2025-08-03 15:07:38 +0200

    authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar lg:mask-b-from-10  md:border-t-1 border-black">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient uppercase ">Resumind</p>
+      </Link>
+      <Link to="/upload">
+        <p className="primary-button w-fit capitalize transition-all duration-500 ease-in-out hover:scale-105 ">
+          Upload Resume <span className="text-green-500 font-semibold">✓</span>
+        </p>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
