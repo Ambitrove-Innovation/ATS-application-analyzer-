64e2512e6c64f44e89ff728d30442ad9bac0e2c8
Kamogelo <ekmogasoa@gmail.com>
2025-08-13 11:26:58 +0200

    updated

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,10 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("app/routes/home.tsx"),
-  route("/auth", "app/routes/auth.tsx"),
-  route("/upload", "app/routes/upload.tsx"),
-  route("/resume/:id", "app/routes/resume.tsx"),
-  route("/wipe", "app/routes/wipe.tsx"),
-  route("*", "app/routes/notFound.tsx"),
+  index("routes/home.tsx"),
+  route("/auth", "/app/routes/auth.tsx"),
+  route("/Upload", "/app/routes/upload.tsx"),
+  route("/resume/:id", "/app/routes/resume.tsx"),
+  route("/wipe", "/app/routes/wipe.tsx"),
+  route("*", "/app/routes/notFound.tsx"),
 ] satisfies RouteConfig;
